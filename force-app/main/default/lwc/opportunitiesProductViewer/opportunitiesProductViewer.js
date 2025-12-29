b4b6c3a43321c9fcd34d4df821cf4378e181890d
JessicaMoukoko <moukokojessica316@gmail.com>
2025-12-29 20:22:24 +0100

    LWC modifications and Apex Classes

--- force-app/main/default/lwc/opportunitiesProductViewer/opportunitiesProductViewer.js
+++ force-app/main/default/lwc/opportunitiesProductViewer/opportunitiesProductViewer.js
@@ -1,17 +1,43 @@
-import { LightningElement } from 'lwc';
-import OpportunityProductController from '@salesforce/apex/OpportuniutyProductController.getOpportunityProducts';
+import { LightningElement, api, wire  } from 'lwc';  
+import getOpportunityProducts from '@salesforce/apex/OpportunityProductController.getOpportunityProducts';
+import getUserProfileName from '@salesforce/apex/ProfileIdentifier.getUserProfileName';
 
-export default class OpportunitiesProductViewer extends LightningElement {}
+export default class OpportunitiesProductViewer extends LightningElement {
 
-    
+@api recordId;
+isAdmin = false;
+isCommercial = false;
+opportunities;
+error;
+profileError;
+wiredOpportunitiesProductsResults;  
+
 columns = [
-        { label: 'Nom du Produit', fieldName: 'Product2.Name', type: 'text' },
-        { label: 'Prix Unitaire', fieldName: 'UnitPrice', type: 'Number' },
-        { label: 'Prix Total', fieldName: 'TotalPrice', type: 'Number' },
-        { label: 'Quantité', fieldName: 'Quantity', type: 'Number' },
-        { label: 'Quantité en Stock', fieldName: 'QuantityInStock__c', type: 'Number' }
+        { label: 'Nom du Produit', fieldName: 'ProductName', type: 'text' },
+        { label: 'Prix Unitaire', fieldName: 'UnitPrice', type: 'number' },
+        { label: 'Prix Total', fieldName: 'TotalPrice', type: 'number' },
+        { label: 'Quantité', fieldName: 'Quantity', type: 'number' },
+        { label: 'Quantité en Stock', fieldName: 'QuantityInStock__c', type: 'number' }
     ];
 
 
-        
-    
+    @wire(getOpportunityProducts,{ OpportunityId: '$recordId' })
+    wiredOpportunities(result) {
+        this.wiredOpportunitiesProductsResults = result;
+        if (result.data) {
+            this.opportunities = result.data;
+        } else if (result.error) {
+            this.error = result.error;
+        }
+    }
+
+    @wire(getUserProfileName)
+wiredProfile({ data, profileError }) {
+    if (data) {
+        this.isAdmin = data === 'System Administrator';
+        this.isCommercial = data === 'Custom: Sales Profile';
+    } else if (profileError) {
+        this.profileError = profileError;
+    }
+}
+}
